29a5be1a1a52c56fa9877d9c9bb418a79a44ae1f
Amna Naseer <amnanaseer924@gmail.com>
2025-01-07 19:33:03 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var section = document.getElementById("skills");
+var sec = document.getElementById("skills_h");
+sec === null || sec === void 0
+  ? void 0
+  : sec.addEventListener("click", function () {
+      section === null || section === void 0
+        ? void 0
+        : section.classList.toggle("hide");
+    });
---
+++ main.ts
@@ -0,0 +1,6 @@
+let section = document.getElementById("skills");
+let sec = document.getElementById("skills_h");
+
+sec?.addEventListener("click", () => {
+  section?.classList.toggle("hide");
+});
